02c3e56028793da2894d339c9e9aeede46a8cf5f
fitur <fitur@users.noreply.github.com>
2019-08-19 15:56:11 +0200

    Update and rename Get-GPOPerSetting to Get-GPOPerSetting.ps1

--- Scripts/Get-GPOPerSetting.ps1
+++ Scripts/Get-GPOPerSetting.ps1
@@ -1,12 +1,21 @@
 Import-Module GroupPolicy
 
 $UnlinkedGPOList = @()
+$LinkedGPOList = @()
 
+# Gather unlinked group policies with specific
 Get-GPO -All | ForEach-Object {
 	if ($_ | Get-GPOReport -ReportType XML | Where-Object { ($_ | Select-String -Pattern "Screen saver timeout") -and ($_ | Select-String -Pattern "<LinksTo>") })
 	{
 		$UnlinkedGPOList += $_ | Select-Object DisplayName, Owner, CreationTime, ModificationTime
 	}
+}
+
+# Get linked group policies
+Get-GPO -All | ForEach-Object {
+    if ($_ | Get-GPOReport -ReportType Xml | Where-Object {$_ | Select-String -Pattern "<SOMPath>xxx.local/Data/Accounts/Computers</SOMPath>"}) {
+        $GPOList += ($_ | Select-Object DisplayName, Owner, CreationTime, ModificationTime)
+    }
 }
 
 $UnlinkedGPOList | Out-GridView
