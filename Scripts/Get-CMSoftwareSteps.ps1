c2b45742162aa41fe1f4d819188b29a66afdd131
fitur <fitur@users.noreply.github.com>
2019-08-20 08:34:00 +0200

    Update Get-CMSoftwareSteps.ps1

--- Scripts/Get-CMSoftwareSteps.ps1
+++ Scripts/Get-CMSoftwareSteps.ps1
@@ -1,3 +1,6 @@
 $TSApps = @()
+
 Get-CMTaskSequenceStepInstallApplication -TaskSequenceId PS100157 | Where-Object {$_.Properties.AppInfo.DisplayName -notlike ""} | ForEach-Object {$TSApps += $_.Properties.AppInfo.DisplayName}
-$TSSteps = Get-CMTaskSequenceStep -TaskSequenceId PS100157 | Where-Object { ($_.SmsProviderObjectPath -eq "SMS_TaskSequence_InstallSoftwareAction") -or ($_.SmsProviderObjectPath -eq "SMS_TaskSequence_InstallApplicationAction") } | Select-Object *
+Get-CMTaskSequenceStepInstallSoftware -TaskSequenceId PS100157 | Where-Object {$_.PackageID -match $SiteCode} | ForEach-Object {$TSApps += (Get-CMPackage -Id $_.PackageID | Select-Object -ExpandProperty Name)}
+
+$TSApps | Select-Object -Unique | Out-GridView
