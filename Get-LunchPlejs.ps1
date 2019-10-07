3c133c56efc54aa1be39a6200c01eacf01b8bc86
Peter Olausson <peter.olausson@advania.com>
2019-10-07 10:27:29 +0200

    ALt + Shift + F :)

--- Get-LunchPlejs.ps1
+++ Get-LunchPlejs.ps1
@@ -1,6 +1,6 @@
 # Variables
 $Rating = 3.5 # Minimum rating
-$Address = "Fredsborgsgatan 24, Liljeholmen" -replace " ","+" # Address of starting point
+$Address = "Fredsborgsgatan 24, Liljeholmen" -replace " ", "+" # Address of starting point
 $Radius = 1000 # Meters radius
 $Random = 5 # Number of random objects to add to poll
 $PollText = '/poll "Vart käkar vi på fredag?"' # Poll text
@@ -11,7 +11,7 @@ $DayOfWeek = (Get-Date).DayOfWeek.value__ # Set to 5 if not run on friday
 
 # Gather from Google Maps
 $Location = (Invoke-RestMethod -Method Get -Uri "https://maps.googleapis.com/maps/api/geocode/json?address=$($Address)&key=$($GoogleMaps_API_Key)").Results | Select-Object -First 1
-$Coordinates = "{0},{1}" -f (($Location.geometry.location.lat -replace ",","."), ($Location.geometry.location.lng -replace ",","."))
+$Coordinates = "{0},{1}" -f (($Location.geometry.location.lat -replace ",", "."), ($Location.geometry.location.lng -replace ",", "."))
 $Nearby = (Invoke-RestMethod -Method Get -Uri "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$($Coordinates)&radius=$($Radius)&types=restaurant&key=$($GoogleMaps_API_Key)").Results
 
 # Placeholders
@@ -25,16 +25,16 @@ if (($Nearby | Measure-Object).Count -gt 0) {
 }
 
 # Remove places without opening_hours
-$Places | Where-Object {(!$_.opening_hours.periods[$($DayOfWeek)].open.time)} | ForEach-Object {$Places.Remove($_) | Out-Null}
+$Places | Where-Object { (!$_.opening_hours.periods[$($DayOfWeek)].open.time) } | ForEach-Object { $Places.Remove($_) | Out-Null }
 
 # Filter detailed array and select $number random objects
 $Places | Where-Object {
-    ($_.opening_hours.periods[$($DayOfWeek)].open.time).SubString(0,2) -le $LunchTime -and # Opening hours before $LunchTime
-    ($_.opening_hours.periods[$($DayOfWeek)].close.time).SubString(0,2) -gt $($LunchTime + 2) -and # Opening hours after $LunchTime + 2 hours
+    ($_.opening_hours.periods[$($DayOfWeek)].open.time).SubString(0, 2) -le $LunchTime -and # Opening hours before $LunchTime
+    ($_.opening_hours.periods[$($DayOfWeek)].close.time).SubString(0, 2) -gt $($LunchTime + 2) -and # Opening hours after $LunchTime + 2 hours
     ($_.rating -ge $Rating) # Rating equal to or above $Rating
 } | Get-Random -Count $Random | ForEach-Object -Process {
     $_ | Add-Member -MemberType NoteProperty -Name "distance" -Value (Invoke-RestMethod -Method Get -Uri "https://maps.googleapis.com/maps/api/distancematrix/json?origins=$($Coordinates)&destinations=$($_.formatted_address -replace " ","+")&mode=$($TransportType)&units=metric&key=$($GoogleMaps_API_Key)")
-    $PollText = $PollText + (' "{0} - Rating: {1} - Avstånd: {2} (URL: {3}) - Öppettider: {4} - {5}"' -f $_.Name, $_.Rating, $_.distance.rows.elements.distance.text, $_.Website, ($_.opening_hours.periods[$((Get-Date).DayOfWeek.value__)].open.time).SubString(0,2), ($_.opening_hours.periods[$((Get-Date).DayOfWeek.value__)].close.time).SubString(0,2))
+    $PollText = $PollText + (' "{0} - Rating: {1} - Avstånd: {2} (URL: {3}) - Öppettider: {4} - {5}"' -f $_.Name, $_.Rating, $_.distance.rows.elements.distance.text, $_.Website, ($_.opening_hours.periods[$((Get-Date).DayOfWeek.value__)].open.time).SubString(0, 2), ($_.opening_hours.periods[$((Get-Date).DayOfWeek.value__)].close.time).SubString(0, 2))
 }
 
 # Set poll to clipboard to paste into Slack
