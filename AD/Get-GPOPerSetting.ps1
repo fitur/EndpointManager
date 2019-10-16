71392408714f93051ff967a2e297f87ca4994e78
Peter Olausson <peter.olausson@advania.com>
2019-10-16 14:00:46 +0200

    Added if to counter missing AD sync

--- AD/Get-GPOPerSetting.ps1
+++ AD/Get-GPOPerSetting.ps1
@@ -5,17 +5,23 @@ $LinkedGPOList = @()
 
 # Gather unlinked group policies with specific
 Get-GPO -All | ForEach-Object {
-	if ($_ | Get-GPOReport -ReportType XML | Where-Object { ($_ | Select-String -Pattern "Screen saver timeout") -and ($_ | Select-String -Pattern "<LinksTo>") })
-	{
+	if ($_ | Get-GPOReport -ReportType XML | Where-Object { ($_ | Select-String -Pattern "Screen saver timeout") -and ($_ | Select-String -Pattern "<LinksTo>") }) {
 		$UnlinkedGPOList += $_ | Select-Object DisplayName, Owner, CreationTime, ModificationTime
 	}
 }
 
 # Get linked group policies
 Get-GPO -All | ForEach-Object {
-    if ($_ | Get-GPOReport -ReportType Xml | Where-Object {$_ | Select-String -Pattern "<SOMPath>xxx.local/Data/Accounts/Computers</SOMPath>"}) {
-        $GPOList += ($_ | Select-Object DisplayName, Owner, CreationTime, ModificationTime)
-    }
+	if ($_ | Get-GPOReport -ReportType Xml | Where-Object { $_ | Select-String -Pattern "<SOMPath>xxx.local/Data/Accounts/Computers</SOMPath>" }) {
+		$GPOList += ($_ | Select-Object DisplayName, Owner, CreationTime, ModificationTime)
+	}
 }
 
-$UnlinkedGPOList | Out-GridView
+# get group policies with specific keyword
+Get-GPO -All | ForEach-Object -Begin { $GPOList = New-Object -TypeName System.Collections.ArrayList } {
+	if ($_ | Get-GPOReport -ReportType XML | Where-Object { ($_ | Select-String -Pattern "Screen saver timeout") }) {
+		[void]$GPOList.Add(( $_ | Select-Object DisplayName, Owner, CreationTime, ModificationTime ))
+	}
+}
+
+$GPOList | Out-GridView
--- SCCM/Create-DeviceSpecificCollections.ps1
+++ SCCM/Create-DeviceSpecificCollections.ps1
@@ -5,8 +5,18 @@ function Import-CMEnvironment {
         Set-location $SiteCode":" -ErrorAction Stop
     }
     catch [System.Exception] {
-        Write-Warning  -Message $_.Exception.Message
+        Write-Warning -Message $_.Exception.Message
     }
+}
+
+function Show-Feedback {
+    param (
+        # Parameter help description
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Text
+    )
+    Write-Output ("{0}: {1}" -f (Get-Date -Format g), $Text)
 }
 
 function Get-CMDeviceModels {
@@ -20,17 +30,11 @@ function Get-CMDeviceModels {
                 (
                     ($_.Model -notmatch "Virtual") -and
                     ($_.Model -notlike "")
-                ) -and
-                (
-                    ($_.Manufacturer -match "Dell") -or
-                    ($_.Manufacturer -match "Hewlett") -or
-                    ($_.Manufacturer -match "HP") -or
-                    ($_.Manufacturer -match "Microsoft")
                 )
             }
         }
         catch [System.Exception] {
-            Write-Warning  -Message $_.Exception.Message
+            Write-Warning -Message $_.Exception.Message
         }
     }
     end {
@@ -60,25 +64,65 @@ function New-CMLimitingCollection {
             ValueFromPipeline = $true,
             HelpMessage = "Name of limiting collection.")]
         [ValidateNotNullOrEmpty()]
-        [string]$CollectionName = "All Active Computers Last $ActiveDays Days"
+        [string]$CollectionName = "All Active Computers Last $ActiveDays Days",
+
+        # Specifies the name of the limiting collection sub-directory
+        [Parameter(Mandatory = $false,
+            ValueFromPipeline = $true,
+            HelpMessage = "Name of limiting collection sub-directory.")]
+        [ValidateNotNullOrEmpty()]
+        [string]$CollectionSubDirectory = "Limiting Collections"
     )
+    begin {
+        ## Load environment
+        Import-CMEnvironment
+
+        ## Get collection info
+        $Collection = Get-CMCollection -Name $CollectionName -ErrorAction SilentlyContinue
+    }
     process {
         try {
-            if ($null -eq ($NewCollection = Get-CMCollection -Name $CollectionName)) {
-                $Query = "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System where SMS_R_System.Name in (select Name from SMS_R_System where ((DATEDIFF(day, SMS_R_SYSTEM.AgentTime, getdate()) <=$ActiveDays) and AgentName = 'SMS_AD_SYSTEM_DISCOVERY_AGENT')) and SMS_R_System.Name in (select Name from SMS_R_System where ((DATEDIFF(day, SMS_R_SYSTEM.AgentTime, getdate()) <=$ActiveDays) and AgentName = 'Heartbeat Discovery'))"
-                New-CMCollection -Name $CollectionName -CollectionType Device -LimitingCollectionId $LimitingCollection -Comment "Online last $ActiveDays days" -RefreshSchedule (New-CMSchedule -Start "$((Get-Date).Year)-01-01" -RecurInterval Days -RecurCount 1) -RefreshType Periodic -ErrorAction Stop
+            if ($null -eq $Collection) {
+                ## Create new collection
+                $Collection = New-CMCollection -Name $CollectionName -CollectionType Device -LimitingCollectionId $LimitingCollection -Comment "Online last $ActiveDays days" -RefreshSchedule (New-CMSchedule -Start "$((Get-Date).Year)-01-01" -RecurInterval Days -RecurCount 1) -RefreshType Periodic -ErrorAction Stop
+
+                ## Add query rule to collection
+                if ((Get-CMDiscoveryMethod | Where-Object {$_.ComponentName -eq "SMS_AD_SYSTEM_DISCOVERY_AGENT"} | Select-Object -ExpandProperty Flag) -eq 6) {
+                    $Query = "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System where SMS_R_System.Name in (select Name from SMS_R_System where ((DATEDIFF(day, SMS_R_SYSTEM.AgentTime, getdate()) <=$ActiveDays) and AgentName = 'SMS_AD_SYSTEM_DISCOVERY_AGENT')) and SMS_R_System.Name in (select Name from SMS_R_System where ((DATEDIFF(day, SMS_R_SYSTEM.AgentTime, getdate()) <=$ActiveDays) and AgentName = 'Heartbeat Discovery'))"
+                } else {
+                    $Query = "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System where SMS_R_System.Name in (select Name from SMS_R_System where ((DATEDIFF(day, SMS_R_SYSTEM.AgentTime, getdate()) <=$ActiveDays) and AgentName = 'Heartbeat Discovery'))"
+                }
                 Add-CMDeviceCollectionQueryMembershipRule -CollectionName $CollectionName -RuleName "Heartbeat last $ActiveDays days" -QueryExpression $Query -ErrorAction Stop
+
+                ## Create limiting collection sub-directory if required
+                if (!(Get-Item -Path $SiteCode":\DeviceCollection\$CollectionSubDirectory" -ErrorAction SilentlyContinue)) {
+                    try {
+                        ## Create new limiting collection sub-directory
+                        New-Item -Path $SiteCode":\DeviceCollection" -Name $CollectionSubDirectory -ItemType Directory -ErrorAction Stop
+                    }
+                    catch [System.Exception] {
+                        Write-Warning -Message $_.Exception.Message
+                    }
+                }
+                try {
+                    ## Move collection to new sub-directory
+                    Move-CMObject -ObjectId $Collection.CollectionID -FolderPath $SiteCode":\DeviceCollection\$CollectionSubDirectory" -ErrorAction Stop
+                }
+                catch [System.Exception] {
+                    Write-Warning -Message $_.Exception.Message
+                }
+                
             }
             else {
-                throw "Collection ""$CollectionName"" already exist. Returing information."
+                throw "Collection ""$CollectionName"" already exist. Returning information to pipeline."
             }
         }
         catch [System.Exception] {
-            Write-Warning  -Message $_.Exception.Message
+            Write-Warning -Message $_.Exception.Message
         }
     }
     end {
-        return $NewCollection
+        return $Collection
     }
 }
 
@@ -86,32 +130,38 @@ function New-CMDeviceCollectionPerModel {
     [CmdletBinding()]
     param (
         # Specifies the name of the limiting collection
-        [Parameter(Mandatory = $true,
-        ValueFromPipeline = $true,
-        HelpMessage = "Array of model objects")]
+        [Parameter(Mandatory = $false,
+            ValueFromPipeline = $true,
+            HelpMessage = "Array of model objects")]
         [ValidateNotNullOrEmpty()]
-        $Models,
+        $Models = (Get-CMDeviceModels),
 
         # Specifies the name of the sub-directory
-        [Parameter(Mandatory = $true,
-        ValueFromPipeline = $true,
-        HelpMessage = "CM sub-directory")]
+        [Parameter(Mandatory = $false,
+            ValueFromPipeline = $true,
+            HelpMessage = "CM root sub-directory")]
         [ValidateNotNullOrEmpty()]
-        $SubDirectory        
+        $SubDirectory = "Hardware Collections",
+
+        # Specifies which limiting collection to use
+        [Parameter(Mandatory = $false,
+            ValueFromPipeline = $true,
+            HelpMessage = "Specify CollectionID of limiting collection, else use -LimitingCollection (New-CMLimitingCollection <parameters>).CollectionID")]
+        [ValidateNotNullOrEmpty()]
+        $LimitingCollection = (New-CMLimitingCollection).CollectionID
     )
     
     begin {
         ## Load environment
         Import-CMEnvironment
         
-        ## Create new environmental variables
+        ## Get current collections to compare and avoid creating duplicates
         $CurrentCollections = Get-WmiObject -ComputerName $SiteCode.SiteServer -Namespace "root\SMS\site_$($SiteCode)" -Query 'select Name, LimitToCollectionID from SMS_Collection'
-        $LimitingCollection = New-CMLimitingCollection
     }
     
     process {
         $Models | ForEach-Object {
-            ## Gather manufacturer details and rename
+            ## Gather manufacturer details, rename and set Manufacturer variable
             switch -Wildcard ($_.Manufacturer) {
                 "Dell*" {
                     $Manufacturer = "Dell"
@@ -125,9 +175,15 @@ function New-CMDeviceCollectionPerModel {
                 "Microsoft*" {
                     $Manufacturer = "Microsoft"
                 }
+                "Lenovo*" {
+                    $Manufacturer = "Lenovo"
+                }
+                "Asus*" {
+                    $Manufacturer = "Asus"
+                }
             }
         
-            ## Gather model details and rename
+            ## Gather model details, rename if required and set Model variable
             switch -Wildcard ($_.Model) {
                 "*HP*" {
                     $Model = $_ -replace "HP ", ""
@@ -137,22 +193,60 @@ function New-CMDeviceCollectionPerModel {
                 }
             }
         
-            ## Create device string for naming purposes
+            ## Create device string for collection naming purpose
             $DeviceCollection = ("{0} {1}" -f $Manufacturer, $Model)
         
             ## Create collection if not exists
-            if ($DeviceCollection -notin $CurrentCollections.Name) {
-        
-                ## Inform which device is being parsed
-                Write-Host "Creating collection for $DeviceCollection"
-        
-                $Collection = New-CMDeviceCollection -Name $DeviceCollection -LimitingCollectionId $LimitingCollection.CollectionID -RefreshSchedule (New-CMSchedule -Start "$((Get-Date).Year)-01-01" -RecurInterval Days -RecurCount 1) -RefreshType Periodic -ErrorAction Stop
-        
-                ## Add query to above collection
-                Add-CMDeviceCollectionQueryMembershipRule -CollectionId $Collection.CollectionID -RuleName $_.Model -QueryExpression "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceID = SMS_R_System.ResourceId where SMS_G_System_COMPUTER_SYSTEM.Manufacturer = ""$($_.Manufacturer)"" and SMS_G_System_COMPUTER_SYSTEM.Model = ""$($_.Model)""" -ErrorAction Stop
-                
-                ## Move collection to new sub-directory
-                Move-CMObject -ObjectId $Collection.CollectionID -FolderPath $SiteCode":\DeviceCollection\$SubDirectory" -ErrorAction SilentlyContinue
+            if ($DeviceCollection -notin $CurrentCollections.Name) {                
+                try {
+                    ## Show which device is being parsed
+                    Show-Feedback -Text "Parsing $($DeviceCollection) - ($([int]$Models.IndexOf($_)+1) of $($Models.Count))"
+
+                    ## Attempt to create device specific CM collection
+                    Show-Feedback -Text "Creating collection for $($DeviceCollection)"
+                    $Collection = New-CMDeviceCollection -Name $DeviceCollection -LimitingCollectionId $LimitingCollection -RefreshSchedule (New-CMSchedule -Start "$((Get-Date).Year)-01-01" -RecurInterval Days -RecurCount 1) -RefreshType Periodic -ErrorAction Stop
+            
+                    ## Add query rule to above collection
+                    Show-Feedback -Text "Adding query rule to $($Collection.Name)"
+                    $Query = "select SMS_R_SYSTEM.ResourceID,SMS_R_SYSTEM.ResourceType,SMS_R_SYSTEM.Name,SMS_R_SYSTEM.SMSUniqueIdentifier,SMS_R_SYSTEM.ResourceDomainORWorkgroup,SMS_R_SYSTEM.Client from SMS_R_System inner join SMS_G_System_COMPUTER_SYSTEM on SMS_G_System_COMPUTER_SYSTEM.ResourceID = SMS_R_System.ResourceId where SMS_G_System_COMPUTER_SYSTEM.Manufacturer = ""$($_.Manufacturer)"" and SMS_G_System_COMPUTER_SYSTEM.Model = ""$($_.Model)"""
+                    Add-CMDeviceCollectionQueryMembershipRule -CollectionId $Collection.CollectionID -RuleName $_.Model -QueryExpression $Query -ErrorAction Stop
+                    
+                    ## Create root and manufacturer specific sub-directory if required
+                    if (!(Get-Item -Path $SiteCode":\DeviceCollection\$SubDirectory\$Manufacturer" -ErrorAction SilentlyContinue)) {
+                        try {
+                            if (!(Get-Item -Path $SiteCode":\DeviceCollection\$SubDirectory" -ErrorAction SilentlyContinue)) {
+                                ## Create new root sub-directory
+                                Show-Feedback -Text "Creating new console directory $($SubDirectory)"
+                                New-Item -Path $SiteCode":\DeviceCollection" -Name $SubDirectory -ItemType Directory -ErrorAction Stop
+                            }
+                            ## Create new manufacturer specific sub-directory
+                            Show-Feedback -Text "Creating new manufacturer specific console directory for $($Manufacturer)"
+                            New-Item -Path $SiteCode":\DeviceCollection\$SubDirectory" -Name $Manufacturer -ItemType Directory -ErrorAction Stop
+                        }
+                        catch [System.Exception] {
+                            Write-Warning -Message $_.Exception.Message
+                        }
+                    }
+                    try {
+                        ## Move collection to new sub-directory
+                        Show-Feedback -Text "Moving collection $($Collection.CollectionID) to $($Manufacturer)"
+                        Move-CMObject -ObjectId $Collection.CollectionID -FolderPath $SiteCode":\DeviceCollection\$SubDirectory\$Manufacturer" -ErrorAction Stop
+                    }
+                    catch [System.Exception] {
+                        Write-Warning -Message $_.Exception.Message
+                    }
+                }
+                catch [System.Exception] {
+                    Write-Warning -Message $_.Exception.Message
+                }
+
+                ## New line at end of object
+                Show-Feedback -Text "-------------------"
+            }
+            else {
+                ## Show which device is being skipped
+                Show-Feedback -Text "Skipping $($DeviceCollection), already exists - ($([int]$Models.IndexOf($_)+1) of $($Models.Count))"
+                Show-Feedback -Text "-------------------"
             }
         }
     }
