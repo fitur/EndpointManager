73a7211d145ccd941f5283ba2442eabeb72e084e
Peter Olausson <fitur@users.noreply.github.com>
2019-11-11 14:51:56 +0100

    Update Get-MDTInfo.ps1

--- OSD/Get-MDTInfo.ps1
+++ OSD/Get-MDTInfo.ps1
@@ -1,13 +1,14 @@
 $URI = "http://lolol.local/ConfigMgrWebService/ConfigMgr.asmx"
 $SecretKey = "138be014-268a-4a15-ae06-4f39271bfbb8"
+$MAC = "04:0E:3C:9D:3F:FB"
 
 $WebService = New-WebServiceProxy -Uri $URI
 
-$Computer = $WebService.GetMDTComputerByName($SecretKey,"WS07768")
+$Computer = $WebService.GetMDTComputerByMacAddress($SecretKey,$MAC)
 if (($Computer | Measure-Object).Count -eq 1) {
-    $Name = $Computer.ComputerName
-    $Roles = $WebService.GetMDTDetailedComputerRoleMembership($SecretKey,$($Computer.ComputerIdentity))
-    $User = $WebService.GetADComputerAttributeValue($SecretKey,"WS07768","ManagedBy")
+    $Name = $WebService.GetMDTComputerNameByIdentity($SecretKey,$Computer)
+    $Role = $WebService.GetMDTDetailedComputerRoleMembership($SecretKey,$Computer) | Where-Object {$_.RoleID -ne 2}
+    $User = $WebService.GetADComputerAttributeValue($SecretKey,$Name,"ManagedBy")
     $Domain = ($WebService.GetADDomain($SecretKey)).DomainName.Split(".")[0]
 }
 
@@ -15,5 +16,4 @@ if (($Computer | Measure-Object).Count -eq 1) {
 
 if ($User -match "(?<name>[a-z]{3,6}[0-9]{3}){1}") {
     $Combination = "{0}\{1}" -f $Domain, $Matches.name
-    Add-CMUserAffinityToDevice -UserName $Combination -DeviceName $Name
 }
