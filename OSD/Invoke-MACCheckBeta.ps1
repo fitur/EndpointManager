90ecdcd058b3ba2529b3112013c377c0364422ad
Peter Olausson <peter.olausson@advania.com>
2019-11-14 22:53:19 +0100

    First Commit

---
+++ OSD/Invoke-MACCheckBeta.ps1
@@ -0,0 +1,29 @@
+﻿$URI = "http://sccm07.katalog.local/ConfigMgrWebService/ConfigMgr.asmx"
+$SecretKey = "5b48f57b-0d36-43dd-a40b-8133a11a7d8d"
+$MAC = "04:0E:3C:9D:3F:FB"
+$MAC3 = "04:0E:3C:9D:3F:FA"
+$MAC1 = "04:0E:3C:9D:3F:FC"
+$MACAddress = "C4:65:16:05:E6:87"
+
+$WebService = New-WebServiceProxy -Uri $URI -ErrorAction SilentlyContinue
+$TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment
+
+$Computer = $WebService.GetMDTComputerByMacAddress($SecretKey,$MAC)
+if (($Computer | Measure-Object).Count -eq 1) {
+    $Name = $WebService.GetMDTComputerNameByIdentity($SecretKey,$Computer)
+    $Role = $WebService.GetADComputerAttributeValue($SecretKey,$Name,"type")
+    $User = $WebService.GetADComputerAttributeValue($SecretKey,$Name,"ManagedBy")
+    $Domain = ($WebService.GetADDomain($SecretKey)).DomainName.Split(".")[0]
+}
+
+
+if ($User -match "(?<name>[a-z]{3,6}[0-9]{3}){1}") {
+    $NameCombination = "{0}\{1}" -f $Domain, $Matches.name
+    $TSEnvironment.Value("SMSTSUDAUsers") = $NameCombination
+}
+
+switch ($Role.RoleName) {
+    "Administrative" { $TSEnvironment.Value("Solnarole") = "Role_ADM" }
+    "Educational" { $TSEnvironment.Value("Solnarole") = "Role_EDU" }
+    "Public" { $TSEnvironment.Value("Solnarole") = "Role_Publik" }
+}
---
+++ SCCM/Invoke-EmptyCollectionRemoval.ps1
@@ -0,0 +1,33 @@
+﻿Get-CMCollection -CollectionType Device | Where-Object { ($_.MemberCount -lt 10) -and ($_.IsReferenceCollection -eq $false) -and ($_.IsBuiltIn -eq $false) -and ($_.LastChangeTime -lt (Get-Date).AddMonths(-10)) } | ForEach-Object -Begin { $RetiredCollections = New-Object -TypeName System.Collections.ArrayList } -Process {
+    Write-Host "Evaluating collection: $($_.Name)"
+
+    $TempCollection = $_
+    if ([string]::IsNullOrEmpty((Get-CMDeployment | Where-Object { $_.CollectionID -eq $TempCollection.CollectionID }))) {
+        
+        # Export CM collection
+        try {
+            Export-CMCollection -CollectionId $_.CollectionID -ExportFilePath "\\sccm07\e$\Backup\Collections\Device Collections\AutoExport\$($_.Name).mof" -ExportComment "$(Get-Date -Format d): Collection Cleanup ($($_.CollectionType)); " -Force -ErrorAction SilentlyContinue
+        }
+        catch [System.Exception] {
+            Write-Warning "Failed to export collection: $($_.Name)"
+        }
+
+        # Remove CM collection
+        try {
+            Remove-CMCollection -Id $_.CollectionID -Force -ErrorAction SilentlyContinue
+        }
+        catch [System.Exception] {
+            Write-Warning "Failed to remove collection: $($_.Name)"
+        }
+
+        # Add data to array
+        [void]$RetiredCollections.Add([PSCustomObject]@{
+                Name           = $TempCollection.Name
+                CollectionID   = $TempCollection.CollectionID
+                LastChangeTime = $TempCollection.LastChangeTime
+        })
+    }
+
+    # Remove temporary variable
+    Remove-Variable TempCollection -Force -ErrorAction SilentlyContinue
+} -End { $RetiredCollections | Export-Csv -Path "filesystem::\\sccm07\e$\Backup\Collections\Device Collections\AutoExport\AE-$(New-Guid).csv" -NoTypeInformation -Encoding UTF8 -Force }
