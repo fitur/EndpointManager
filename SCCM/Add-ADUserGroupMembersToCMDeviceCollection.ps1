a8090410d85bc8d5e80ac96d792829d4d86fac47
Peter Olausson <fitur@users.noreply.github.com>
2020-06-18 10:45:34 +0200

    Update Add-ADUserGroupMembersToCMDeviceCollection.ps1

--- SCCM/Add-ADUserGroupMembersToCMDeviceCollection.ps1
+++ SCCM/Add-ADUserGroupMembersToCMDeviceCollection.ps1
@@ -1,13 +1,13 @@
-$URI = "http://*/ConfigMgrWebService/ConfigMgr.asmx"
-$SecretKey = "*"
+$URI = "http://sccm07.katalog.local/ConfigMgrWebService/ConfigMgr.asmx"
+$SecretKey = "5b48f57b-0d36-43dd-a40b-8133a11a7d8d"
 $WS = New-WebServiceProxy -Uri $URI
-$CollectionID = "*"
-$ADGroup = "*"
+$CollectionID = "PS100466"
+$ADGroup = "SF"
 
 Get-ADGroupMember -Identity $ADGroup | ForEach-Object {
     Write-Host "Found $($_.name)"
 
-    $CMComputers = Get-CMUserDeviceAffinity -UserName "KATALOG\$($_.Name)" | Where-Object {($_.Sources -eq 4) -and ($_.Types -eq 1) -and ($_.UniqueUserName -match $_.Name)}
+    $CMComputers = Get-CMUserDeviceAffinity -UserName "KATALOG\$($_.Name)" | Where-Object {(($_.Sources -eq 4) -or ($_.Sources -eq 7)) -and ($_.Types -eq 1) -and ($_.UniqueUserName -match $_.Name)}
     Write-Host "$($_.name) has got $($CMComputers | Measure-Object | Select-Object -ExpandProperty Count) computer(s): $($CMComputers.ResourceName)"
 
     foreach ($CMComputer in $CMComputers) {
