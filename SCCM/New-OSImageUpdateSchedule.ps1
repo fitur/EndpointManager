6f599121c132fb889d29cacdbf89002edbac2111
Peter Olausson <fitur@users.noreply.github.com>
2021-03-23 18:58:18 +0100

    Update New-OSImageUpdateSchedule.ps1

--- SCCM/New-OSImageUpdateSchedule.ps1
+++ SCCM/New-OSImageUpdateSchedule.ps1
@@ -1,53 +1,48 @@
-#
-# Press 'F5' to run this script. Running this script will load the ConfigurationManager
-# module for Windows PowerShell and will connect to the site.
-#
-# This script was auto-generated at '2018-11-20 10:02:57'.
-
-# Uncomment the line below if running in an environment where script signing is 
-# required.
-#Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
-
-# Site configuration
-$SiteCode = "C01" # Site code 
-$ProviderMachineName = "SESCCMSRV04.tobii.intra" # SMS Provider machine name
-
-# Customizations
-$initParams = @{}
-#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
-#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
-
-# Do not change anything below this line
-
-# Import the ConfigurationManager.psd1 module 
-if((Get-Module ConfigurationManager) -eq $null) {
-    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+begin {
+    try {
+        # Load CM module
+        Import-module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1') -ErrorAction Stop
+        $SiteCode = Get-PSDrive -PSProvider CMSITE -ErrorAction Stop
+        Set-location $SiteCode":" -ErrorAction Stop
+    }
+    catch [System.Exception] {
+        Write-Warning -Message $_.Exception.Message
+    }
 }
+process {
+    $UNC = Get-CMCollection -Id "SMS000US"
+    $Updates = Get-CMSoftwareUpdate -IsDeployed $true -IsExpired $false -IsSuperseded $false -Fast | Where-Object {$_.LocalizedCategoryInstanceNames -notcontains "Upgrades"}
 
-# Connect to the site's drive if it is not already present
-if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
-    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
-}
+    # Create schedule (saturday = 6)
+    $i = 0
+    do {
+        $i++
+        $Schedule = [System.DateTime]::Today.AddDays($i)
+    } until ((Get-Date).AddDays($i).DayOfWeek.value__ -eq 6) #(saturday = 6)
 
-# Set the current location to be the site code.
-Set-Location "$($SiteCode):\" @initParams
+    # Gather OSD-images from all task sequences in production
+    Get-CMTaskSequenceDeployment -CollectionId $UNC.CollectionID -Summary | ForEach-Object -Begin {$OSDImage = New-Object -TypeName System.Collections.ArrayList} -Process {
+        Get-CMTaskSequenceStepApplyOperatingSystem -TaskSequenceId $_.PackageID | Select-Object -ExpandProperty ImagePackageID | ForEach-Object -Begin {$Temp = New-Object -TypeName System.Collections.ArrayList} -Process {
+            $Temp.Add($_)
+        }
 
+        # Process only unique
+        $Temp | Select-Object -Unique | ForEach-Object {
+            $TempOSDImage = Get-CMOperatingSystemImage -Id $_ -ErrorAction SilentlyContinue
+            if (![string]::IsNullOrWhiteSpace($TempOSDImage)) {
+                $OSDImage.Add($TempOSDImage)
+            }
+        }
+    }
 
-$Splat = @{
-    "ErrorAction" = "Stop"
-    "Verbose" = $true ## set to true if you need output
-}
+    $OSDImage | ForEach-Object {
+        switch -Wildcard ($_.ImageOSVersion) {
+            "10.0.19041.*" { $OSDImageOSVersion = "20H2" }
+            "10.0.18362.*" { $OSDImageOSVersion = "1909" }
+            "10.0.17762.*" { $OSDImageOSVersion = "1809" }
+        }
 
-$i = 0
-do {
-    $i++
-    $Schedule = [System.DateTime]::Today.AddDays($i)
-} until ((Get-Date).AddDays($i).DayOfWeek.value__ -eq 6)
-
-$SoftwareUpdates = Get-CMSoftwareUpdateGroup -Name ("ADR*{0}*" -f (Get-Date -UFormat "%Y-%m")) @Splat | ForEach-Object {
-    Get-CMSoftwareUpdate -UpdateGroupId $_.CI_ID -Fast @Splat
-}
-
-Get-CMOperatingSystemImage | Sort-Object -Descending Version @Splat | ForEach-Object {
-    $_ | New-CMOperatingSystemImageUpdateSchedule -SoftwareUpdate $SoftwareUpdates -CustomSchedule $Schedule -ContinueOnError $true -UpdateDistributionPoint $true @Splat
-}
+        # Inject all updates into 
+        New-CMOperatingSystemImageUpdateSchedule -Id $_.PackageID -ContinueOnError $false -RemoveSupersededUpdates $true -SoftwareUpdate ($Updates | Where-Object {$_.LocalizedDisplayName -match $OSDImageOSVersion}) -UpdateDistributionPoint $true -RunNow
+    }
+} 
