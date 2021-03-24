919e126fdbd34db37dcfecb513b530df41a2b57f
Peter Olausson <fitur@users.noreply.github.com>
2021-03-24 10:51:21 +0100

    Update Invoke-HPIARepoUpdateLite.ps1
    Moved sync outside per device loop and added Set-RepositoryConfiguration -Setting OnRemoteFileNotFound -Value LogAndContinue to each repository.

--- Add-ons/Invoke-HPIARepoUpdateLite.ps1
+++ Add-ons/Invoke-HPIARepoUpdateLite.ps1
@@ -5,7 +5,7 @@ $Models = Import-Csv (Join-Path -Path ($RepoDir | Split-Path -Parent) -ChildPath
 Get-ChildItem -Path $RepoDir -Name "10.0.*" | ForEach-Object {
     switch ($_) {
         "10.0.19042" { $HPIAOSNumber = "2009" }
-        "10.0.17763" { $HPIAOSNumber = "1809"}
+        "10.0.17763" { $HPIAOSNumber = "1809" }
     }
 
     # Step into directory
@@ -15,10 +15,17 @@ Get-ChildItem -Path $RepoDir -Name "10.0.*" | ForEach-Object {
     Initialize-Repository -ErrorAction Stop
 
     # Add repository filter, run sync and clean repo for each model
-    foreach ($Model in $Models) {
+    foreach ($Model in ($Models | Sort-Object -Unique ProdCode | Where-Object {$_.Model -match "HP"})) {
+        Write-Verbose -Verbose "Attempting to synchronize $($Model.Model) for OS-version $HPIAOSNumber."
         Add-RepositoryFilter -Platform $Model.ProdCode -Category Bios,Firmware,Driver,Software -Characteristic SSM -Os win10 -OsVer $HPIAOSNumber -ReleaseType Recommended -ErrorAction SilentlyContinue
-        Invoke-RepositorySync -Quiet -ErrorAction SilentlyContinue
-        Invoke-RepositoryCleanup -ErrorAction SilentlyContinue
+        #Invoke-RepositorySync -Quiet -ErrorAction SilentlyContinue
+        #Invoke-RepositoryCleanup -ErrorAction SilentlyContinue
+        #Remove-RepositoryFilter -Platform $Model.ProdCode -Yes
+        Write-Verbose -Verbose "------------------------------------"
     }
 
+    Set-RepositoryConfiguration -Setting OnRemoteFileNotFound -Value LogAndContinue
+    Invoke-RepositorySync -Quiet -ErrorAction SilentlyContinue
+    Invoke-RepositoryCleanup -ErrorAction SilentlyContinue
+    #Remove-RepositoryFilter -Platform $Model.ProdCode -Yes
 } 
