d9b8128f8fa5b980b5acb59d5ef20fe2fd77c58e
Peter Olausson <fitur@users.noreply.github.com>
2021-05-20 08:57:10 +0200

    Create Invoke-RedistributeFailed.ps1

---
+++ SCCM/Invoke-RedistributeFailed.ps1
@@ -0,0 +1,8 @@
+Import-module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1') -ErrorAction Stop
+$SiteCode = Get-PSDrive -PSProvider CMSITE -ErrorAction Stop
+
+foreach ($Package in (Get-WmiObject -Namespace root/sms/site_$($SiteCode.SiteCode) -Class SMS_PackageStatusDistPointsSummarizer -ComputerName $SiteCode.SiteServer | Where-Object {$_.State -eq 2})) {
+    $DP = Get-WmiObject -Namespace root/sms/site_$($SiteCode.SiteCode) -Class SMS_DistributionPoint -ComputerName $SiteCode.SiteServer | Where-Object {($_.PackageID -eq $Package.PackageID) -and ($_.ServerNALPath -eq $Package.ServerNALPath)}
+    $DP.RefreshNow = $true
+    $DP.Put()
+} 
