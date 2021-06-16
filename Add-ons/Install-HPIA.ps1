b6306cb7df96948fd2d44808a358efb28a6516ed
Peter Olausson <fitur@users.noreply.github.com>
2021-06-16 16:26:19 +0200

    Update Install-HPIA.ps1

--- Add-ons/Install-HPIA.ps1
+++ Add-ons/Install-HPIA.ps1
@@ -1,26 +1,35 @@
- # Modules
+# Variables
+$CMPackageName = "HP Image Assistant Binary Package"
+
+# Modules
 Import-Module ($Env:SMS_ADMIN_UI_PATH.Substring(0, $Env:SMS_ADMIN_UI_PATH.Length - 5) + '\ConfigurationManager.psd1') -ErrorAction Stop
 
 # CM values
 $script:SiteCode = Get-PSDrive -PSProvider CMSITE -ErrorAction Stop
 Set-Location -Path $SiteCode":" -ErrorAction Stop
-$HPIAPackage = Get-CMPackage -Name "HP Image Assistant Binary Package" -Fast -ErrorAction Stop
+
+if ($null -eq (Get-CMPackage -name $CMPackageName -Fast)) {
+    $HPIABinaryPath = Read-Host -Prompt "Path to save HPIA binary files"
+    $HPIAPackage = New-CMPackage -Name $CMPackageName -Path $HPIABinaryPath -ErrorAction Stop
+} else {
+    $HPIAPackage = Get-CMPackage -Name $CMPackageName -Fast -ErrorAction Stop
+    $HPIABinaryPath = $HPIAPackage.PkgSourcePath
+}
 
 # HPIA variables
 $URI = "http://ftp.hp.com/pub/caps-softpaq/cmit/HPIA.html"
 $WebReq = Invoke-WebRequest -Uri $URI -ErrorAction Stop
 $SoftPaqURL = $WebReq.Links | Where-Object {$_.href -match "exe"} | Select-Object -ExpandProperty href
 $SoftPaqDownloadPath = "{0}\{1}" -f $env:TEMP, ($SoftPaqURL | Split-Path -Leaf)
+$HPIAVersion = (($WebReq.AllElements | Where-Object {($_.tagname -eq 'tr') -and ($_.innerHTML -match $SoftPaqURL)}).innerHTML -split "`n" | Select-Object -First 1) -replace "<TD>","" -replace "</TD>",""
+$HPIADescription = (($WebReq.AllElements | Where-Object {($_.tagname -eq 'tr') -and ($_.innerHTML -match $SoftPaqURL)}).innerHTML -split "`n" | Select-Object -Skip 1 -First 1) -replace "<TD>","" -replace "</TD>",""
+$HPIAInstallParams = '-f"{0}" -s -e' -f $HPIABinaryPath
+
+# Run installer
 Start-BitsTransfer -Source $SoftPaqURL -Destination $SoftPaqDownloadPath
 $HPIAInstallExecutable = Get-Item $SoftPaqDownloadPath -ErrorAction Stop
-$HPIABinaryPath = $HPIAPackage.PkgSourcePath
-
-$HPIAVersion = (($WebReq.AllElements | Where-Object {($_.tagname -eq 'tr') -and ($_.innerHTML -match $SoftPaqURL)}).innerHTML -split "`n" | Select-Object -First 1) -replace "<TD>","" -replace "</TD>",""
-
-# Run updater
-$HPIAInstallParams = '-f"{0}" -s -e' -f $HPIABinaryPath
 Start-Process -FilePath $HPIAInstallExecutable.FullName -ArgumentList $HPIAInstallParams -Wait -ErrorAction Stop
-$HPIAPackage | Set-CMPackage -Version $HPIAVersion
+$HPIAPackage | Set-CMPackage -Version $HPIAVersion -Description $HPIADescription
 
 # Distribute CM package
-$HPIAPackage | Invoke-CMContentRedistribution 
+$HPIAPackage | Invoke-CMContentRedistribution -DistributionPointName (Get-CMDistributionPointInfo | Sort-Object -Descending Groupcount | Select-Object -First 1 -ExpandProperty Name)
