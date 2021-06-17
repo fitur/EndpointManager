8aaf093a0e6f754106004c68bc843076b0b9334d
Peter Olausson <fitur@users.noreply.github.com>
2021-06-17 16:47:43 +0200

    Update Invoke-HPIAUpdate.ps1

--- Add-ons/Invoke-HPIAUpdate.ps1
+++ Add-ons/Invoke-HPIAUpdate.ps1
@@ -1,22 +1,28 @@
+[CmdletBinding()]
 param (
-    [Parameter(Mandatory = $true)]
-    [ValidateNotNullOrEmpty()]
-    $RepoDir,
+    # Central repository UNC path
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
-    $ReportDir = (Join-Path -Path $env:SystemDrive -ChildPath "HPIA\Logs"),
+    [System.IO.FileInfo]$RepoDir = (Join-Path -Path $env:SystemDrive -ChildPath "HPIA\Downloads"),
+
+    # Log directory
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
-    $UpdateType = "Live",
+    [System.IO.FileInfo]$LogDir = (Join-Path -Path $env:SystemDrive -ChildPath "HPIA\Logs"),
+
+    # Log name
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
-    $PWBin = (Get-ChildItem -Filter *.bin | Select-Object -ExpandProperty FullName),
+    [String]$LogName = "Invoke-HPIAUpdate.log",
+
+    # Update type decides visibility, central repository or online, etc.
     [Parameter(Mandatory = $false)]
-    [ValidateNotNullOrEmpty()]
-    $LogName = "Invoke-HPIAUpdate.log",
+    [ValidateSet("Online","Offline","OSD","Live")]
+    [String]$UpdateType = "Online",
+
+    # HP BIOS password bin file
     [Parameter(Mandatory = $false)]
-    [ValidateNotNullOrEmpty()]
-    $LogsDirectory = (Join-Path -Path $env:SystemRoot -ChildPath "Temp")
+    [System.IO.FileInfo]$PWBin = (Get-ChildItem -Filter *.bin | Select-Object -ExpandProperty FullName)
 )
 begin {
     # Create functions
@@ -34,7 +40,7 @@ begin {
 			[string]$FileName = $LogName
 		)
 		# Determine log file location
-        $LogFilePath = Join-Path -Path $LogsDirectory -ChildPath $FileName
+        $LogFilePath = (Join-Path -Path $LogDir -ChildPath $FileName)
 		
 		# Construct time stamp for log entry
 		$Time = -join @((Get-Date -Format "HH:mm:ss.fff"), "+", (Get-WmiObject -Class Win32_TimeZone | Select-Object -ExpandProperty Bias))
@@ -58,46 +64,44 @@ begin {
 	}
 
     # Create variables
-    $ArgumentString = '/Operation:Analyze /Action:Install /Selection:All /Noninteractive /SoftpaqDownloadFolder:"{0}" /Debug' -f ($ReportDir | Split-Path -Parent)
+    $ArgumentString = '/Operation:Analyze /Selection:All /Noninteractive /Silent /SoftpaqDownloadFolder:"{0}" /Debug' -f (Join-Path -Path $env:SystemDrive -ChildPath "HPIA\Downloads")
 
     # Create HPIA argument string if BIOS password exist
     if ($null -ne $PWBin) {
-        Write-CMLogEntry -Value "HP BIOS password detected. Adjusting argument string." -Severity 2
+        Write-CMLogEntry -Value "HP BIOS password bin file detected. Adjusting argument string." -Severity 2
         $ArgumentString = ($ArgumentString + ' /BIOSPwdFile:"{0}"' -f $PWBin)
-    }
-    else {
-        Write-CMLogEntry -Value "No HP BIOS password detected." -Severity 1
+    } else {
+        Write-CMLogEntry -Value "No HP BIOS password bin file detected." -Severity 1
     }
 
-    # Switch for UpdateType (background or interactive)
+    # Update type switch
     switch ($UpdateType) {
-        "Live" { $ArgumentString = ($ArgumentString + '  /Offlinemode:"{0}" /ReportFolder:"{1}"' -f $RepoDir, $ReportDir) }
-        "Online" { $ArgumentString = ($ArgumentString + ' /Silent /ReportFolder:"{0}"' -f $ReportDir) }
-        "DriversOnly" { $ArgumentString = ($ArgumentString + ' /Silent /Category:Drivers,Software /Offlinemode:"{0}" /ReportFolder:"{1}"' -f $RepoDir, $ReportDir) }
-        "BIOSOnly" { $ArgumentString = ($ArgumentString + ' /Silent /Category:BIOS,Firmware /Offlinemode:"{0}" /ReportFolder:"{1}"' -f $RepoDir, $ReportDir) }
-        "OSD" {
-            Write-CMLogEntry -Value "Running in OSD mode. Log path re-evaluated to %_SMSTSLogPath%" -Severity 1
-
-            # Construct argument string
-            $ArgumentString = ($ArgumentString + ' /Silent /Category:Drivers,Software,Firmware /Offlinemode:"{0}" /ReportFolder:"%_SMSTSLogPath%"' -f $RepoDir)
-        }
+        Online  { $ArgumentString = ($ArgumentString + ' /Action:Install /ReportFolder:"{0}"' -f $LogDir) }
+        Offline { $ArgumentString = ($ArgumentString + ' /Action:Install /Offlinemode:"{0}" /ReportFolder:"{1}"' -f $RepoDir, $LogDir) }
+        OSD     { $ArgumentString = ($ArgumentString + ' /Action:Install /Offlinemode:"{0}" /ReportFolder:"%_SMSTSLogPath%"' -f $RepoDir) }
+        List    { $ArgumentString = ($ArgumentString + ' /Action:List /ReportFolder:"{0}"' -f $LogDir) }
     }
 }
 process {
-    if (Test-Path -Path $RepoDir) {
-        # Run HP Image Assistant
-        try {
-            Write-CMLogEntry -Value "Running HPIA with the following argument string: $ArgumentString" -Severity 1
-            Start-Process '.\HPImageAssistant.exe' -ArgumentList $ArgumentString -Wait -ErrorAction Stop
-            Wait-Process -ProcessName HPImageAssistant* -ErrorAction SilentlyContinue
-            exit 0;
+    # Run HP Image Assistant
+    try {
+        Write-CMLogEntry -Value "Running HPIA with the following argument string: $ArgumentString" -Severity 1
+        $Process = Start-Process ".\HPImageAssistant.exe" -ArgumentList $ArgumentString -Wait -PassThru -ErrorAction Stop
+        
+        # Switch depending on HPIA return code
+        switch ($Process.ExitCode) {
+            0 { Write-CMLogEntry -Value "Successfully updated." -Severity 1; exit 0 }
+            256 { Write-CMLogEntry -Value "The analysis returned no recommendations." -Severity 1; exit 0 }
+            257 { Write-CMLogEntry -Value "There were no recommendations selected for the analysis." -Severity 1; exit 0 }
+            3010 { Write-CMLogEntry -Value "Install Reboot Required - SoftPaq installations are successful, and at least one requires a reboot." -Severity 1; exit 3010 }
+            3020 { Write-CMLogEntry -Value "Install failed — One or more SoftPaq installations failed." -Severity 2; exit 1 }
+            4096 { Write-CMLogEntry -Value "The platform is not supported." -Severity 2; exit 1 }
+            4097 { Write-CMLogEntry -Value "The parameters are invalid." -Severity 2; exit 1 }
+            4098 { Write-CMLogEntry -Value "There is no Internet connection." -Severity 2; exit 1 }
+            Default { Write-CMLogEntry -Value "Return code $($Process.ExitCode). Please refer to https://ftp.ext.hp.com/pub/caps-softpaq/cmit/whitepapers/HPIAUserGuide.pdf." -Severity 1; exit 0 }
         }
-        catch [System.SystemException] {
-            Write-CMLogEntry -Value "Error - Could not run HPIA. Message: $($_.Exception.Message)" -Severity 3
-            exit 1;
-        }
-    } else {
-        Write-CMLogEntry -Value "Error - Directory $($RepoDir) not available." -Severity 2
-        exit 2;
+    }
+    catch [System.SystemException] {
+        Write-CMLogEntry -Value "Error - Could not run HPIA. Message: $($_.Exception.Message)" -Severity 3; exit 1
     }
 }
