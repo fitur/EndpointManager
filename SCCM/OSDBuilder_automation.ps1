9d7ed07b89d64de263b1b04dd3e64cb013ccb927
Peter Olausson <fitur@users.noreply.github.com>
2022-02-17 14:07:10 +0100

    Update OSDBuilder_automation.ps1

--- SCCM/OSDBuilder_automation.ps1
+++ SCCM/OSDBuilder_automation.ps1
@@ -1,8 +1,9 @@
 ## Variables
-$OSDBuilderPath = "D:\Scripts and Tools\OSDBuilder"
-$ISOPath = "D:\Scripts and Tools\ISO"
+$OSDBuilderPath = "D:\OSDBuilder"
+$ISOPath = "D:\OSDBuilder\ISO"
 $CMOSPath = "\\ne\system\SCCM\OS\OS Images\MS Client"
 $CMTSPrefix = "NLTG - Pilot"
+$SiteCode = Get-PSDrive -PSProvider CMSITE -ErrorAction Stop
 
 ## Install, import and initalize module
 try {
@@ -20,7 +21,7 @@ try {
     #OSDBuilder -Update
 
     ## Setup
-    Initialize-OSDBuilder
+    Initialize-OSDBuilder -SetHome $OSDBuilderPath
     Get-OSDBuilder -SetHome $OSDBuilderPath -Initialize -CreatePaths -Download OneDrive
 }
 catch [System.Exception] {
@@ -32,13 +33,12 @@ Get-Volume | Where-Object {($_.DriveType -eq "CD-ROM") -and ($_.OperationalStatu
     Get-DiskImage -DevicePath ($_.Path).Substring(0, ($_.Path.Length - 1)) | Where-Object {$_.ImagePath -like "$ISOPath*"} | Dismount-DiskImage -Verbose
 }
 
-## Process each ISO
+## Import OS from ISO if not already present
 foreach ($OSImage in (Get-ChildItem -Path $ISOPath -Filter "*.ISO" -Recurse -File)) {
     ## Variables
     $OSImage.FullName -match "(?<version>2\dH(1|2))" | Out-Null
     $OSVersion = $Matches.version
     $TaskName = ("Windows 10 Enterprise x64 {0}" -f $OSVersion)
-    $SiteCode = Get-PSDrive -PSProvider CMSITE -ErrorAction Stop
 
     ## Find imported OS media
     if (!(Get-OSMedia -OSReleaseId $OSVersion -Newest x64)) {
@@ -61,9 +61,17 @@ foreach ($OSImage in (Get-ChildItem -Path $ISOPath -Filter "*.ISO" -Recurse -Fil
         ## Create OS build task
         Get-OSMedia -OSReleaseId $OSVersion -Newest x64 | New-OSBuildTask -TaskName $TaskName -EnableNetFx3
     }
+}
+
+## Process each OS media import
+foreach ($OSMedia in (Get-OSMedia -Revision OK)) {
+
+    ## Variables
+    $OSVersion = $OSMedia.ReleaseId
+    $TaskName = ("Windows 10 Enterprise x64 {0}" -f $OSMedia.ReleaseId)
 
     ## Download OS image updates
-    Get-OSMedia -OSReleaseId $OSVersion -Newest | Update-OSMedia -Download -Execute #-SkipComponentCleanup
+    $OSMedia | Update-OSMedia -Download -Execute #-SkipComponentCleanup
 
     ## Cleanup superseeded updates
     Get-DownOSDBuilder -Superseded Remove
@@ -80,7 +88,7 @@ foreach ($OSImage in (Get-ChildItem -Path $ISOPath -Filter "*.ISO" -Recurse -Fil
         $WIMFile = Get-Item -Path ("{0}\OS\sources\install.wim" -f $OSBuild.FullName)
 
         ## Copy WIM file to CMOSPath and rename file to TaskName
-        Copy-Item -Path $WIMFile -Destination $CMOSPath -Force -Verbose
+        Move-Item -Path $WIMFile -Destination $CMOSPath -Force -Verbose
         $NewName = Rename-Item -Path (Get-Item -Path ("{0}\{1}" -f $CMOSPath, $WIMFile.Name)) -NewName "$(("{0} {1} {2} {3} {4}" -f $OSBuild.ImageName, $OSBuild.Arch, $OSBuild.ReleaseId, $OSBuild.UBR, (Get-Random -Minimum 1000 -Maximum 9999)) -replace " ","_").wim" -Force -PassThru
 
         ## Call CM environment
@@ -89,9 +97,10 @@ foreach ($OSImage in (Get-ChildItem -Path $ISOPath -Filter "*.ISO" -Recurse -Fil
         ## Import OS image into CM
         $CMOSImage = New-CMOperatingSystemImage -Name ("{0} {1} {2}" -f $OSBuild.ImageName, $OSBuild.Arch, $OSBuild.ReleaseId) -Version $OSBuild.UBR -Description $OSBuild.ModifiedTime -Path $NewName -ErrorAction Stop -Verbose
         $CMOSImage | Start-CMContentDistribution -DistributionPointGroupName (Get-CMDistributionPointGroup | Sort-Object MemberCount -Descending | Select-Object -First 1 -ExpandProperty Name) -ErrorAction Stop -Verbose
+        $CMOSImage | Move-CMObject -FolderPath '.\OperatingSystemImage\MS Client' -Verbose
 
         ## Edit task sequence
-        Get-CMTaskSequence -Name ("{0} - {1} - {2}" -f $CMTSPrefix, $OSBuild.OperatingSystem, $OSVersion) -Fast -ErrorAction Stop | Set-CMTaskSequenceStepApplyOperatingSystem -ImagePackage $CMOSImage -ImagePackageIndex 1
+        #Get-CMTaskSequence -Name ("{0} - {1} - {2}" -f $CMTSPrefix, $OSBuild.OperatingSystem, $OSVersion) -Fast -ErrorAction Stop | Set-CMTaskSequenceStepApplyOperatingSystem -ImagePackage $CMOSImage -ImagePackageIndex 1
 
         ## Exit CM environment
         Set-Location -Path $env:SystemDrive -ErrorAction Stop
