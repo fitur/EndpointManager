c6ae8f791e9d8cfe2321e4d4e81ba561b3c75893
Peter Olausson <peter.olausson@nltg.com>
2022-09-27 09:29:04 +0200

    Update to parameters

--- ConfigManager/Update-DPCert.ps1
+++ ConfigManager/Update-DPCert.ps1
@@ -1,6 +1,18 @@
+try {
+    # Load CM module
+    Import-module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1') -ErrorAction Stop
+    $SiteCode = Get-PSDrive -PSProvider CMSITE -ErrorAction Stop
+    Set-location $SiteCode":" -ErrorAction Stop
+}
+catch [System.Exception] {
+    Write-Warning -Message $_.Exception.Message
+}
+
+# Edit certificate parameters
 $CertPath = "\\path\to\certfile.pfx"
 $CertPW = "password-in-plain-text" | ConvertTo-SecureString -AsPlainText -Force
 
-Get-CMDistributionPoint | Where-Object {$_.EmbeddedProperties.CertificateFile.Value1 -ne $CertPath} | ForEach-Object {
+# Run import on all distributionpoints which currently use a certificate
+Get-CMDistributionPoint | Where-Object {($_.EmbeddedProperties.CertificateFile.Value1) -and ($_.EmbeddedProperties.CertificateFile.Value1 -ne $CertPath)} | ForEach-Object {
     $_ | Set-CMDistributionPoint -CertificatePath $CertPath -CertificatePassword $CertPW -UserDeviceAffinity AllowWithAutomaticApproval -Force -WarningAction SilentlyContinue
-}
+} 
