759f8c3f2e1d5222548174f184b5c6e0e9f204c3
Peter Olausson <peter.olausson@nltg.com>
2022-09-27 09:48:04 +0200

    changes

--- ConfigManager/Update-DPCert.ps1
+++ ConfigManager/Update-DPCert.ps1
@@ -1,5 +1,29 @@
+<#
+
+.SYNOPSIS
+    PowerShell script to update certificate on all CM distribution points.
+
+.EXAMPLE
+    .\Update-DPCert.ps1
+
+.DESCRIPTION
+    This PowerShell script is supposed to be manually run whenever a certificate is pending renewal.
+
+.LINK
+    https://docs.microsoft.com/en-us/mem/analytics/proactive-remediations
+
+.NOTES
+    Version:        1.0
+    Creation Date:  September 27, 2022
+    Last Updated:   September 27, 2022
+    Author:         Peter Olausson
+    Contact:        admin@fitur.se
+    Web Site:       https://github.com/fitur
+
+#>
+
+# Attempt to load CM module
 try {
-    # Load CM module
     Import-module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1') -ErrorAction Stop
     $SiteCode = Get-PSDrive -PSProvider CMSITE -ErrorAction Stop
     Set-location $SiteCode":" -ErrorAction Stop
