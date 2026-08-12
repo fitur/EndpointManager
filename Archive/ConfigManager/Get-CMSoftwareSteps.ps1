ce7ce11194f8584036ce2dfbe67c6cf297db5158
Peter Olausson <peter.olausson@gmail.com>
2026-08-12 11:28:19 +0200

    Remove obsolete PowerShell scripts related to ConfigMgr and system management.

--- Archive/ConfigManager/Get-CMSoftwareSteps.ps1
+++
@@ -1,6 +0,0 @@
-$TSApps = @()
-
-Get-CMTaskSequenceStepInstallApplication -TaskSequenceId PS100157 | Where-Object {$_.Properties.AppInfo.DisplayName -notlike ""} | ForEach-Object {$TSApps += $_.Properties.AppInfo.DisplayName}
-Get-CMTaskSequenceStepInstallSoftware -TaskSequenceId PS100157 | Where-Object {$_.PackageID -match $SiteCode} | ForEach-Object {$TSApps += (Get-CMPackage -Id $_.PackageID | Select-Object -ExpandProperty Name)}
-
-$TSApps | Select-Object -Unique | Out-GridView
